338849cf7586b903cb1f79adb4faced7d37ab6f7
xx <2356804168@qq.com>
2019-11-25 13:48:53 +0800

    上传提交springboot

---
+++ src/main/java/com/crazycode/SpringbootgitApplication.java
@@ -0,0 +1,13 @@
+package com.crazycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootgitApplication.class, args);
+    }
+
+}
